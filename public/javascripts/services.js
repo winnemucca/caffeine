0402485615799ebdd3d64a63cc430cb30740b64d
Michael Herman <hermanmu@gmail.com>
2015-06-05 22:33:50 -0600

    code cleanup

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,79 +1,79 @@
 
 app.factory('Drink',function($http) {
-	var Drink = function(name,date,caffeineLevel) {
- 		this.name = name;
- 		// this.date = moment(date).format('L');
- 		this.data = date;
- 		this.caffeineLevel = caffeineLevel;
+  var Drink = function(name,date,caffeineLevel) {
+    this.name = name;
+    // this.date = moment(date).format('L');
+    this.data = date;
+    this.caffeineLevel = caffeineLevel;
 
- 	};
-	return Drink;
+  };
+  return Drink;
 });
 
 
 
 app.factory('DrinkLibrary',function($http,Drink){
-	
-	return {
-		getDrinks:getDrinks,
-		addDrink: addDrink,
-		deleteDrink: deleteDrink,
-		updateDrink:updateDrink
-	};
 
-	function getDrinks(){
-		return $http.get('/api/drinks').
-		success(function(data){
-			console.log(data);
-			return data;
-		}).
-		error(function(data){
-			console.log('error');
-		})
-	}
+  return {
+    getDrinks:getDrinks,
+    addDrink: addDrink,
+    deleteDrink: deleteDrink,
+    updateDrink:updateDrink
+  };
 
-	function addDrink(drink) {
-		// var allDrinkList = [];
-		// this.allDrinkList.push(drink);
-		return $http.post('/api/drinks',drink).
-		success(function(data){
-		// this.allDrinkList.push(data);
+  function getDrinks(){
+    return $http.get('/api/drinks').
+    success(function(data){
+      console.log(data);
+      return data;
+    }).
+    error(function(data){
+      console.log('error');
+    });
+  }
 
-			// allDrinkList.push(data);
-			console.log(data);
-			return data;
-		}).
-		error(function(data){
-			console.log('error');
-		});
-	}
+  function addDrink(drink) {
+    // var allDrinkList = [];
+    // this.allDrinkList.push(drink);
+    return $http.post('/api/drinks',drink).
+    success(function(data){
+    // this.allDrinkList.push(data);
 
-	function deleteDrink(id){
-		return $http.delete('/api/drink/'+id).
-		success(function(data){
-			console.log(data);
-			return data;
-		}).
-		error(function(data){
-			console.log('error');
-		});
-	}
+      // allDrinkList.push(data);
+      console.log(data);
+      return data;
+    }).
+    error(function(data){
+      console.log('error');
+    });
+  }
 
-	function updateDrink(id){
-		return $http.put('/api/drink/'+id).
-			success(function(data){
-				console.log(data);
-				return data;
-			}).
-			error(function(data){
-				console.log('error');
-			});
-	}
+  function deleteDrink(id){
+    return $http.delete('/api/drink/'+id).
+    success(function(data){
+      console.log(data);
+      return data;
+    }).
+    error(function(data){
+      console.log('error');
+    });
+  }
+
+  function updateDrink(id){
+    return $http.put('/api/drink/'+id).
+      success(function(data){
+        console.log(data);
+        return data;
+      }).
+      error(function(data){
+        console.log('error');
+      });
+  }
 });
 
 
 app.service('modalService',['$modal',function($modal){
-	var modalDefaults = {
+  var modalDefaults = {
             backdrop: true,
             keyboard: true,
             modalFade: true,
@@ -113,7 +113,7 @@ app.service('modalService',['$modal',function($modal){
                     $scope.modalOptions.close = function (result) {
                         $modalInstance.dismiss('cancel');
                     };
-                }
+                };
             }
 
             return $modal.open(tempModalDefaults).result;
