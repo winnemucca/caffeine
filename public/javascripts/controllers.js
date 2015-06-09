44e6fb9e5a916af48001eef1f8611d4a2efdb38c
Steven Kauyedauty <skauyedauty@gmail.com>
2015-06-09 05:28:11 -0600

    bin/www

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,3 +1,4 @@
+var app = angular.module('myApp');
 app.controller('homeController',function($scope){
   $scope.greeting = 'hello world';
 });
--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,3 +1,4 @@
+var app = angular.module('myApp');
 app.factory('Drink',function($http) {
   var Drink = function(name,date,caffeineLevel) {
     this.name = name;
