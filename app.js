d76fea611f71c2357aa2db873f4dcbd2f0af3139
Steven Kauyedauty <skauyedauty@gmail.com>
2015-07-12 20:01:54 -0600

    added port to app.js was in bin/www

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ var app = express();
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+app.set('port', (process.env.PORT || 3000));
+
 
 var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/caffeine';
 mongoose.connect(mongoUri, function (err, db) {
