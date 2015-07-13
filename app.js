37b4ac34220dbf27c6c229200bd2365856eb8e53
Steven Kauyedauty <skauyedauty@gmail.com>
2015-07-12 19:55:24 -0600

    added port to appjs

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ var app = express();
 
 // app.set('port', (process.env.PORT || 5000));
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/caffeine';
 mongoose.connect(mongoUri, function (err, db) {
